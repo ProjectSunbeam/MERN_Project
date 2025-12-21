14ae46db5b8d3fa5f8e7615107f608e2a2a96ff8
Rushikesh Kharage <rushikeshkharage04@gmail.com>
2025-12-21 19:57:21 +0530

    all courses api

--- backend/routes/courses.js
+++ backend/routes/courses.js
@@ -4,31 +4,56 @@ const request = require("../utils/createResult");
 const router = express.Router();
 
 router.get("/",(req,res)=>{
-  const sql = "select * from courses"
+  const sql = "SELECT * FROM courses WHERE CURDATE() BETWEEN start_date AND end_date"
   pool.query(sql,(error,data)=>{
     res.send(request.createResult(error,data));
   })
 })
 
-router.post("/",(req,res)=>{
-  const {course_id,course_name,description,fees,start_date,end_date,video_expire_days} = req.body
-  const sql = "insert into courses(course_id,course_name,description,fees,start_date,end_date,video_expire_days) values(?,?,?,?,?,?,?)";
-  pool.query(sql,[course_id,course_name,description,fees,start_date,end_date,video_expire_days],(error,data)=>{
+router.get("/all-courses", (req, res) => {
+  const { startDate, endDate } = req.query;
+
+  let sql = "SELECT * FROM courses";
+  const params = [];
+
+  if (startDate && endDate) {
+    sql += " WHERE start_date <= ? AND end_date >= ?";
+    params.push(endDate, startDate);
+  }
+
+  pool.query(sql, params, (error, data) => {
+    res.send(request.createResult(error, data));
+  });
+});
+
+// router.post("/courseUser",(req,res)=>{
+//   const {course_name,description,fees,end_date,video_expire_days} = req.body
+//   const sql = "insert into courses(course_name , description , fees , end_date , video_expire_days) values (?,?,?, SYSDATE(),?,?)";
+//   pool.query(sql,[course_name , description , fees , end_date , video_expire_days],(error,data)=>{
+//     res.send(request.createResult(error,data));
+//   })
+// })
+
+router.post("/courseAdmin",(req,res)=>{
+  const {course_name,description,fees,start_date,end_date,video_expire_days} = req.body
+  const sql = "insert into courses(course_name , description , fees , start_date , end_date , video_expire_days) values (?,?,?,?,?,?)";
+  pool.query(sql,[course_name , description , fees , start_date , end_date , video_expire_days],(error,data)=>{
     res.send(request.createResult(error,data));
   })
 })
 
 router.put("/",(req,res)=>{
-  const {fees,course_id} = req.body
-  const sql = "update courses set fees=? where course_id = ?";
-  pool.query(sql,[fees,course_id],(error,data)=>{
+  const {course_name,description,fees,start_date,end_date,video_expire_days,course_id} = req.body
+  const sql = "update courses set course_name =? , description=? , fees=? , start_date=? , end_date=? , video_expire_days=? where course_id = ?";
+  pool.query(sql,[course_name,description,fees,start_date,end_date,video_expire_days,course_id],(error,data)=>{
     res.send(request.createResult(error,data));
   })
 })
 
 router.delete("/",(req,res)=>{
-  const sql = "delete from courses where course_id = 6";
-  pool.query(sql,(error,data)=>{
+  const {course_id} = req.body;
+  const sql = "delete from courses where course_id = ?";
+  pool.query(sql,[course_id],(error,data)=>{
     res.send(request.createResult(error,data));
   })
 })
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 4000;
 
 app.use(express.json())
-app.use(authUser)
+// app.use(authUser)
 app.use('/users',userRouter)
 app.use("/courses", coursesRouter);
 
