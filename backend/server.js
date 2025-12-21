0326137235076968a3f58eb7938c40ae6d87833b
Wajid Latkar <latkarwajid@gmail.com>
2025-12-21 18:33:14 +0530

    new changes made

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const coursesRouter = require("./routes/courses");
+const videosRouter = require("./routes/videos");
 
 const app = express();
 const PORT = 4000;
 
-app.use(express.json())
-app.use("/courses", coursesRouter);
+app.use(express.json());
 
-app.listen(PORT,"localhost",()=>{
+app.use("/courses", coursesRouter);
+app.use("/video", videosRouter);
+
+app.listen(PORT, "localhost", () => {
   console.log(`Server Started At Port ${PORT}`);
-})
+});
