07c52fa672b9708ff95113fc957ad18eca50a6a8
Wajid Latkar <latkarwajid@gmail.com>
2025-12-21 17:12:57 +0530

    created new branch

---
+++ backend/routes/videos.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const pool = require("../db/pool");
+const request = require("../utils/createResult");
+const router = express.Router();
+
+
+module.exports = router;
