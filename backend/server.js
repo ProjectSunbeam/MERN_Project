9208b55e958c4fecaa19a45b123a30d0ba1400c7
Manthan-1795 <koremanthanp@gmail.com>
2025-12-21 19:57:43 +0530

    completed all student api and admins enrolled courses

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const coursesRouter = require("./routes/courses");
+const adminRouter = require("./routes/admin");
+const studentRouter = require("./routes/students");
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json())
+app.use("/admin/",adminRouter);
 app.use("/courses", coursesRouter);
+app.use("/students",studentRouter);
 
 app.listen(PORT,"localhost",()=>{
   console.log(`Server Started At Port ${PORT}`);
-})
+})  
