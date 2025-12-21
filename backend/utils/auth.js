1945cf1abb98e92ce9d801065ac01702282e0e3a
Manthan-1795 <koremanthanp@gmail.com>
2025-12-21 16:11:29 +0530

    added content in auth js

---
+++ backend/utils/auth.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+const result = require('./result');
+const config = require('./config');
+
+function authUser(req,res,next){
+    const path = req.url;
+    if(path == '/users/signup' || path == '/users/signin')
+        next();
+    else{
+        const token = req.headers.token;
+        if(!token){
+            res.send(result.createResult('Token is missing'));
+        }
+        else{
+            try {
+                const payload = jwt.verify(token, config.SECRET);
+
+                req.headers.uid = payload.uid;
+                req.headers.email = payload.email;
+
+                next();
+
+            } catch (ex) {
+                res.send(result.createResult('Token is Invalid'));
+            }
+        }
+    }
+}
+
+module.exports = authUser;
