919dec30618206b8c7246ecdc533d1b218c107dc
Manthan-1795 <koremanthanp@gmail.com>
2025-12-25 22:42:10 +0530

    added roleAuthorization to all role Based API's , All API testing Is Done Everything Is Working Correctly, Just The Empty Response Is Yet To Be Handled

--- backend/routes/videos.js
+++ backend/routes/videos.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const pool = require("../db/pool");
 const request = require("../utils/createResult");
+const { roleAuthorization } = require("../utils/auth");
 const router = express.Router();
 
-
-router.get("/add-videos/:course_id", (req, res) => {
+router.get("/add-videos/:course_id", roleAuthorization, (req, res) => {
   const course_id = req.params.course_id;
 
   const sql = `SELECT * FROM videos WHERE course_id = ?`;
@@ -14,34 +14,37 @@ router.get("/add-videos/:course_id", (req, res) => {
   });
 });
 
-router.post("/add",(req,res) =>{
-    const {course_id,title,youtube_url,description}  = req.body;
-    const sql = `INSERT INTO videos  (course_id,title,youtube_url,description) VALUES(?,?,?,?)`;
-    pool.query(sql,[course_id,title,youtube_url,description],(error,data) => {
-        res.send(request.createResult(error,data))
-    });
+router.post("/add", roleAuthorization, (req, res) => {
+  const { course_id, title, youtube_url, description } = req.body;
+  const sql = `INSERT INTO videos  (course_id,title,youtube_url,description,added_at) VALUES(?,?,?,?,CURDATE())`;
+  pool.query(
+    sql,
+    [course_id, title, youtube_url, description],
+    (error, data) => {
+      res.send(request.createResult(error, data));
+    }
+  );
 });
 
-router.delete("/delete/:video_id",(req,res) =>{
-    const video_id = req.params.video_id;
-    const sql = `DELETE FROM videos WHERE video_id = ?`;
-    pool.query(sql,[video_id],(error,data) => {
-
-        res.send(request.createResult(error,data))
-    });
+router.delete("/delete/:video_id", roleAuthorization, (req, res) => {
+  const video_id = req.params.video_id;
+  const sql = `DELETE FROM videos WHERE video_id = ?`;
+  pool.query(sql, [video_id], (error, data) => {
+    res.send(request.createResult(error, data));
+  });
 });
 
-router.put("/update/:video_id",(req,res) =>{
-    const video_id = req.params.video_id;
-    const {title,youtube_url,description}  = req.body;
-    const sql = `UPDATE videos SET title = ? , youtube_url = ? , description = ? WHERE video_id = ?`;
-    pool.query(sql,[title,youtube_url,description,video_id],(error,data) => {
-
-        res.send(request.createResult(error,data))
-    });
+router.put("/update/:video_id", roleAuthorization, (req, res) => {
+  const video_id = req.params.video_id;
+  const { title, youtube_url, description } = req.body;
+  const sql = `UPDATE videos SET title = ? , youtube_url = ? , description = ? WHERE video_id = ?`;
+  pool.query(
+    sql,
+    [title, youtube_url, description, video_id],
+    (error, data) => {
+      res.send(request.createResult(error, data));
+    }
+  );
 });
-
-
-
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const coursesRouter = require("./routes/courses");
 const adminRouter = require("./routes/admin");
 const studentRouter = require("./routes/students");
-const authUser = require("./utils/auth");
+const { authUser, roleAuthorization } = require("./utils/auth");
 const userRouter = require("./routes/users");
 const videosRouter = require("./routes/videos");
 
@@ -12,12 +12,12 @@ const PORT = 4000;
 app.use(express.json());
 app.use(authUser);
 
-app.use('/users',userRouter);
-app.use("/admin/",adminRouter);
+app.use("/users", userRouter);
+app.use("/admin/", adminRouter);
 app.use("/courses", coursesRouter);
-app.use("/students",studentRouter);
+app.use("/students", studentRouter);
 app.use("/video", videosRouter);
 
 app.listen(PORT, "localhost", () => {
   console.log(`Server Started At Port ${PORT}`);
-})
+});
