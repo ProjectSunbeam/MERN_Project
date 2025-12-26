437dd9153c83bf270032d11fe5cb5867a65d65b0
Manthan-1795 <koremanthanp@gmail.com>
2025-12-26 10:39:49 +0530

    added cors config

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,12 @@ const studentRouter = require("./routes/students");
 const { authUser, roleAuthorization } = require("./utils/auth");
 const userRouter = require("./routes/users");
 const videosRouter = require("./routes/videos");
+const cors = require("cors");
 
 const app = express();
 const PORT = 4000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(authUser);
@@ -21,4 +24,3 @@ app.use("/video", videosRouter);
 app.listen(PORT, "localhost", () => {
   console.log(`Server Started At Port ${PORT}`);
 });
- 
