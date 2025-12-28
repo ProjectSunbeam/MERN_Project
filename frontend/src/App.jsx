077ae31a39afe8e6ef1508e66f44bcbf41ef8248
Akshad <akshadlondhe.official@gmail.com>
2025-12-28 12:15:18 +0530

    Complete navbar with login  and registration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import VideoSection from "./pages/video_section";
 
 function App() {
   return (
     <>
+     <Navbar />
       <Routes>
-        <Route path="/" element={<Register />} />
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} /> */}
+        {/* <Route path="/" element={<Register />} /> */}
+        {/* /* <Route path="/login" element={<Login />} /> */}
+        
+        <Route path="/register" element={<Register />} /> 
+        {/* /* <Route path="/courses/:id/learn" element={<VideoSection />} />  */}
       </Routes>
 
       <ToastContainer
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
