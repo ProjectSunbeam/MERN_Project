2a038282a90350708faa9f253df34899b11282a3
Akshad <akshadlondhe.official@gmail.com>
2025-12-30 16:51:46 +0530

    integrated change password route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ import MyCourses from "./pages/MyCourses";
 import MyCourseVideos from "./pages/MyCourseVideos";
 import AddVideoForm from "./pages/AddVideoForm";
 import ProfileDropdown from "./components/ProfileDropdown";
+import ChangePassword from "./components/ChangePassword";
 export const LoginContext = createContext();
 
 function App() {
@@ -42,6 +43,10 @@ function App() {
           <Route
             path="/profile"
             element={loginStatus ? <Profile /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/change-password"
+            element={loginStatus ? <ChangePassword /> : <Navigate to="/" />}
           />
           <Route
             path="/admin"
