64831ae9e5d8a81074a0af17466cd8c5bb82a76f
Wajid Latkar <latkarwajid@gmail.com>
2025-12-30 23:08:56 +0530

    admin can filter / get all students by course id'

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,14 +4,26 @@ const pool = require("../db/pool");
 const result = require("../utils/createResult");
 const router = express.Router();
 
-router.get("/enrolledstudents", roleAuthorization, (req, res) => {
-  const sql = `
-    select s.name, c.course_id, c.course_name
-    from students s
-    inner join courses c on (c.course_id = s.course_id)`;
 
-  pool.query(sql, (error, data) => {
+router.get("/enrolledstudents", roleAuthorization, (req, res) => {
+  const { course_id } = req.query;
+
+  let sql = `
+    SELECT s.name, c.course_id, c.course_name
+    FROM students s
+    INNER JOIN courses c ON s.course_id = c.course_id
+  `;
+
+  const params = [];
+
+  if (course_id) {
+    sql += " WHERE c.course_id = ?";
+    params.push(course_id);
+  }
+
+  pool.query(sql, params, (error, data) => {
     res.send(result.createResult(error, data));
   });
 });
+
 module.exports = router;
