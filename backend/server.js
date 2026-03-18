a17288b7340a80f6ac458127044b9736a0299ec5
Rushikesh Kharage <rushikeshkharage04@gmail.com>
2026-03-18 16:39:03 +0530

    deploying step

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,34 @@ const express = require("express");
 const coursesRouter = require("./routes/courses");
 const adminRouter = require("./routes/admin");
 const studentRouter = require("./routes/students");
-const { authUser, roleAuthorization } = require("./utils/auth");
+const { authUser } = require("./utils/auth");
 const userRouter = require("./routes/users");
 const videosRouter = require("./routes/videos");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
-const PORT = 4000;
 
-app.use(cors());
+const PORT = process.env.PORT || 4000;
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
+
 app.use(authUser);
 
+// Routes
 app.use("/users", userRouter);
-app.use("/admin/", adminRouter);
-app.use("/courses", coursesRouter);
-app.use("/students", studentRouter);
+app.use("/admin", adminRouter);
+app.use("/courses", authUser, coursesRouter);
+app.use("/students", authUser, studentRouter);
 app.use("/video", videosRouter);
 
-app.listen(PORT, "localhost", () => {
+app.listen(PORT, () => {
   console.log(`Server Started At Port ${PORT}`);
 });
